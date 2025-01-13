2d5b50049478e04d3d0484dd2d68554013d276e3
sansela <santosh.sela@gmail.com>
2025-01-12 23:45:15 -0500

    DELETE content catch block

--- src/index.ts
+++ src/index.ts
@@ -95,16 +95,21 @@ app.get('/api/v1/content', userMiddleware, async(req, res) => {
 });
 
 app.delete('/api/v1/content', async(req, res) => {
-    const contentId = req.body.contentId;
-    await ContentModel.deleteMany({
-        contentId,
-        // @ts-ignore
-        userId: req.userId
-    })
-
-    res.json({
-        message: "deleted"
-    })
+    try{
+        const contentId = req.body.contentId;
+        await ContentModel.deleteMany({
+            contentId,
+            // @ts-ignore
+            userId: req.userId
+        })
+        res.json({
+            message: "deleted"
+        })
+    } catch(e) {
+        res.json({
+            message: "Error deleting the content"
+        });
+    }
 
 });
 
